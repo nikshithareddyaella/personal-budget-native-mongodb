0a62b12281b5972e9d1f45cf06472e1e79e64b91
Nikshith reddy <nikshithaaella123@gmail.com>
2024-10-12 18:55:30 -0400

    Initial commit

---
+++ crud.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const budgetModel = require('./models/budget_schema')
+
+
+let url = 'mongodb://127.0.0.1:27017/graphs';
+
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=>{
+    console.log("Connected to database")
+    // Fetch
+    budgetModel.find({})
+    .then((data)=>{
+
+        
+        console.log(data);
+        mongoose.connection.close();
+    })
+    .catch((connectionError)=>{
+        console.log(connectionError)
+    })
+})
+.catch((connectionError)=>{
+    console.log(connectionError);
+})
